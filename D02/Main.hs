415e18d115477411538734d8e2736984827373d7
Jan van Brügge <supermanitu@gmail.com>
2018-12-02 18:48:22 +0200

    Solve Day 2 part 2

--- D02/Main.hs
+++ D02/Main.hs
@@ -1,9 +1,26 @@
 module D02.Main where
 
 import Data.List (sort, group)
+import Data.Maybe (isJust, listToMaybe)
 import Data.Bifunctor (bimap)
+import Control.Monad ((<=<))
 
 solve :: String -> Int
 solve = uncurry (*) . bimap sum sum . unzip . fmap (split (count 2) (count 3)) . words
     where split f g x = (f x, g x)
           count n = min 1 . length . filter (== n) . fmap length . group . sort
+
+solve2 :: String -> Maybe String
+solve2 = listToMaybe <=< sequenceA . filter isJust . fmap checkSimilar . cartesian . words
+    where cartesian xs = [(x, y) | x <- xs, y <- xs, x /= y]
+
+checkSimilar :: (String, String) -> Maybe String
+checkSimilar (a, b) = go False a b
+    where go _ [] _ = Just ""
+          go _ _ [] = Just ""
+          go False (x:xs) (y:ys)
+              | x == y = (x:) <$> go False xs ys
+              | otherwise = go True xs ys
+          go True (x:xs) (y:ys)
+              | x == y = (x:) <$> go True xs ys
+              | otherwise = Nothing
--- Main.hs
+++ Main.hs
@@ -1,9 +1,11 @@
 module Main where
 
-import D02.Main (solve)
+import Data.Maybe (fromMaybe)
+
+import D02.Main (solve2)
 
 present :: Show a => Maybe a -> String
 present = maybe "Error" show
 
 main :: IO ()
-main = readFile "D02/input.txt" >>= print . solve
+main = readFile "D02/input.txt" >>= putStrLn . fromMaybe "Error" . solve2
