fc248075cb7c17f1217b025c839bb2886d188ad7
Jan van Brügge <supermanitu@gmail.com>
2018-12-02 21:09:14 +0200

    Use Control.Arrow instead of custom function

--- D02/Main.hs
+++ D02/Main.hs
@@ -4,11 +4,11 @@ import Data.List (sort, group)
 import Data.Maybe (isJust, listToMaybe)
 import Data.Bifunctor (bimap)
 import Control.Monad ((<=<))
+import Control.Arrow ((&&&))
 
 solve :: String -> Int
-solve = uncurry (*) . bimap sum sum . unzip . fmap (split (count 2) (count 3)) . words
-    where split f g x = (f x, g x)
-          count n = min 1 . length . filter (== n) . fmap length . group . sort
+solve = uncurry (*) . bimap sum sum . unzip . fmap (count 2 &&& count 3) . words
+    where count n = min 1 . length . filter (== n) . fmap length . group . sort
 
 solve2 :: String -> Maybe String
 solve2 = listToMaybe <=< sequenceA . filter isJust . fmap checkSimilar . cartesian . words
