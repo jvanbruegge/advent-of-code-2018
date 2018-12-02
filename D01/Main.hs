7c47fc92ff7341065e0b171780f93e2bf551f901
Jan van Brügge <supermanitu@gmail.com>
2018-12-02 17:05:19 +0200

    Solve Day 1 part 2

--- D01/Main.hs
+++ D01/Main.hs
@@ -1,6 +1,16 @@
 module D01.Main where
 
 import Text.Read (readMaybe)
+import Control.Monad ((<=<))
+import qualified Data.Set as S
+
+parseInput :: String -> Maybe [Int]
+parseInput = traverse (readMaybe . filter (/= '+')) . words
 
 solve :: String -> Maybe Int
-solve = fmap sum . traverse (readMaybe . filter (/= '+')) . words
+solve = fmap sum . parseInput
+
+solve2 :: String -> Maybe Int
+solve2 = snd . go S.empty <=< fmap (scanl (+) 0 . cycle) . parseInput
+    where go s [] = (s, Nothing)
+          go s (x:xs) = if x `S.member` s then (s, Just x) else go (S.insert x s) xs
--- Main.hs
+++ Main.hs
@@ -1,9 +1,9 @@
 module Main where
 
-import D01.Main (solve)
+import D01.Main (solve2)
 
 present :: Show a => Maybe a -> String
 present = maybe "Error" show
 
 main :: IO ()
-main = readFile "D01/input.txt" >>= putStrLn . present . solve
+main = readFile "D01/input.txt" >>= putStrLn . present . solve2
