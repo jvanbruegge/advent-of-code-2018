051f969edf34f90ca57f4177ed72c75076198666
Jan van Brügge <supermanitu@gmail.com>
2018-12-04 22:02:14 +0200

    Improve performance

--- D03/Main.hs
+++ D03/Main.hs
@@ -1,9 +1,9 @@
 module D03.Main where
 
-import Data.Foldable (foldl')
 import Data.List (sort, group)
 import Text.Read (readMaybe)
 import Control.Monad ((<=<))
+import qualified Data.Set as S
 
 data Claim = Claim {
         claimID :: Int,
@@ -22,15 +22,15 @@ parseInput = traverse parseLine . lines
           toClaim _ = Nothing
           spToSpace c = if c `elem` ['#', '@', ',', ':', 'x'] then ' ' else c
 
-calculateIntersection :: Claim -> Claim -> [(Int, Int)]
-calculateIntersection a b = [(x, y) | x <- [minX..maxX - 1], y <- [minY..maxY - 1]]
+calculateIntersection :: Claim -> Claim -> S.Set (Int, Int) -> S.Set (Int, Int)
+calculateIntersection a b = flip (foldr S.insert)
+            [(x, y) | x <- [minX..maxX - 1], y <- [minY..maxY - 1]]
     where minX = max (left a) (left b)
           maxX = min (left a + width a) (left b + width b)
           minY = max (top a) (top b)
           maxY = min (top a + height a) (top b + height b)
 
 solve :: String -> Maybe Int
-solve = length . group . sort
-            . concatMap (uncurry calculateIntersection) .  cartesian <$< parseInput
+solve = S.size . foldr (uncurry calculateIntersection) S.empty . cartesian <$< parseInput
     where cartesian xs = let l = zip xs [0..]
                          in [(x, y) | (x, i) <- l, y <- fst <$> takeWhile ((< i) . snd) l]
